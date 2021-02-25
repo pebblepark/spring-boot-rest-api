d6043909f891393fdc533a0ef5ebd686bd9d57b9
editpark <crystalpark@sqisoft.com>
2021-02-25 15:25:57 +0900

    ResponseService

---
+++ src/main/java/com/example/restapi/service/ResponseService.java
@@ -0,0 +1,71 @@
+package com.example.restapi.service;
+
+import com.example.restapi.model.response.CommonResult;
+import com.example.restapi.model.response.ListResult;
+import com.example.restapi.model.response.SingleResult;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ResponseService {
+    public enum CommonResponse {
+        SUCCESS(0, "성공하였습니다."),
+        FAIL(-1, "실패하였습니다.");
+
+        int code;
+        String msg;
+
+        CommonResponse(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+    }
+
+    // 단일 결과건 처리
+    public <T> SingleResult<T> getSingleResult(T data) {
+        SingleResult<T> result = new SingleResult<>();
+        result.setData(data);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 다중 결과건 처리
+    public <T>ListResult<T> getListResult(List<T> list) {
+        ListResult<T> result = new ListResult<>();
+        result.setList(list);
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 성공 결과 처리
+    public CommonResult getSuccessResult() {
+        CommonResult result = new CommonResult();
+        setSuccessResult(result);
+        return result;
+    }
+
+    // 실패 결과 처리
+    public CommonResult getFailResult() {
+        CommonResult result = new CommonResult();
+        result.setSuccess(false);
+        result.setCode(CommonResponse.FAIL.getCode());
+        result.setMessage(CommonResponse.FAIL.getMsg());
+        return result;
+    }
+
+    // 결과 모델에 api 요청 성공 데이터 세팅
+    public void setSuccessResult(CommonResult result) {
+        result.setSuccess(true);
+        result.setCode(CommonResponse.SUCCESS.getCode());
+        result.setMessage(CommonResponse.SUCCESS.getMsg());
+    }
+}
