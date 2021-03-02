e71592246fd37ef7e71cb847c2cf0b975a6be48e
pebblepark <subling02@gmail.com>
2021-03-02 15:55:10 +0900

    로그인 예외 추가

--- src/main/java/com/example/restapi/advice/ExceptionAdvice.java
+++ src/main/java/com/example/restapi/advice/ExceptionAdvice.java
@@ -1,5 +1,6 @@
 package com.example.restapi.advice;
 
+import com.example.restapi.advice.exception.CEmailSigninFailedException;
 import com.example.restapi.advice.exception.CUserNotFoundException;
 import com.example.restapi.model.response.CommonResult;
 import com.example.restapi.service.ResponseService;
@@ -29,21 +30,30 @@ public class ExceptionAdvice {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult defaultException(HttpServletRequest request, Exception e) {
-        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
+        // 예외 처리의 메시지를 MessageSource 에서 가져오도록 수정
         return responseService.getFailResult(Integer.valueOf(getMessage("unKnown.code")), getMessage("unKnown.msg"));
     }
+
     @ExceptionHandler(CUserNotFoundException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     protected CommonResult userNotFoundException(HttpServletRequest request, CUserNotFoundException e) {
-        // 예외 처리의 메시지를 MessageSource에서 가져오도록 수정
         return responseService.getFailResult(Integer.valueOf(getMessage("userNotFound.code")), getMessage("userNotFound.msg"));
     }
-    // code정보에 해당하는 메시지를 조회합니다.
+
+    @ExceptionHandler(CEmailSigninFailedException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult emailSigninFailed(HttpServletRequest request, CEmailSigninFailedException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("emailSigninFailed.code")), getMessage("emailSigninFailed.msg"));
+    }
+
+    // code 정보에 해당하는 메시지를 조회
     private String getMessage(String code) {
         return getMessage(code, null);
     }
-    // code정보, 추가 argument로 현재 locale에 맞는 메시지를 조회합니다.
+
+    // code 정보, 추가 argument 로 현재 locale 에 맞는 메시지를 조회합니다.
     private String getMessage(String code, Object[] args) {
         return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
     }
+
 }
