01de0716609c0c5b70f4f2eee981e7cda7aad9de
pebblepark <subling02@gmail.com>
2021-03-05 14:47:01 +0900

    UserController 헤더에 X-AUTH-TOKEN을 인자로 받도록 수정

--- src/main/java/com/example/restapi/controller/v1/UserController.java
+++ src/main/java/com/example/restapi/controller/v1/UserController.java
@@ -7,15 +7,13 @@ import com.example.restapi.model.response.ListResult;
 import com.example.restapi.model.response.SingleResult;
 import com.example.restapi.repo.UserJpaRepo;
 import com.example.restapi.service.ResponseService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 @Log4j2
-@Api(tags = {"1. User"})    // UserController 를 대표하는 최상단 타이틀
+@Api(tags = {"2. User"})    // UserController 를 대표하는 최상단 타이틀
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "/v1")
@@ -24,6 +22,9 @@ public class UserController {
     private final UserJpaRepo userJpaRepo;
     private final ResponseService responseService;
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @ApiOperation(value = "회원 리스트 조회", notes = "모든 회원을 조회한다") // 해당 resource 타이틀과 설명
     @GetMapping(value = "/users")
     public ListResult<User> findAllUser() {
@@ -31,23 +32,18 @@ public class UserController {
         return responseService.getListResult(userJpaRepo.findAll());
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @ApiOperation(value = "회원 단건 조회", notes = "userId로 회원을 조회한다")
     @GetMapping(value = "/user/{msrl}")
     public SingleResult<User> findUser(@ApiParam(value = "회원번호", required = true) @PathVariable long msrl, @ApiParam(value = "언어정보", defaultValue = "ko") @RequestParam(required = false) String lang) throws Exception {
         return responseService.getSingleResult(userJpaRepo.findById(msrl).orElseThrow(CUserNotFoundException::new));
     }
 
-    @ApiOperation(value = "회원 입력", notes = "회원을 입력한다.")
-    @PostMapping(value = "/user")
-    public SingleResult<User> save(@ApiParam(value = "회원아이디", required = true) @RequestParam String uid,      // 파라미터에 대한 설명
-                                   @ApiParam(value = "회원이름", required = true) @RequestParam String name) {
-        User user = User.builder()
-                .uid(uid)
-                .name(name)
-                .build();
-        return responseService.getSingleResult(userJpaRepo.save(user));
-    }
-
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @ApiOperation(value = "회원 수정", notes = "회원 정보를 수정한다.")
     @PutMapping(value = "/user")
     public SingleResult<User> modify(@ApiParam(value = "회원번호", required = true) @RequestParam long msrl,
@@ -58,6 +54,9 @@ public class UserController {
         return responseService.getSingleResult(userJpaRepo.save(user));
     }
 
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "X-AUTH-TOKEN", value = "로그인 성공 후 access_token", required = true, dataType = "String", paramType = "header")
+    })
     @ApiOperation(value = "회원 삭제", notes = "회원을 삭제한다.")
     @DeleteMapping(value = "/user")
     public CommonResult delete(@ApiParam(value = "회원번호", required = true) @RequestParam long msrl) {
