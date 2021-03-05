3a4717329258efe6558d88cee0b2746a88eec3a7
pebblepark <subling02@gmail.com>
2021-03-05 16:00:00 +0900

    ExceptionAdvice에 AccessDeniedException handler 추가

--- src/main/java/com/example/restapi/advice/ExceptionAdvice.java
+++ src/main/java/com/example/restapi/advice/ExceptionAdvice.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -51,6 +52,13 @@ public class ExceptionAdvice {
     protected CommonResult authenticationException(HttpServletRequest request, CustomAuthenticationException e) {
         return responseService.getFailResult(Integer.valueOf(getMessage("entryPointException.code")), getMessage("entryPointException.msg"));
     }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    protected CommonResult accessDeniedException(HttpServletRequest request, AccessDeniedException e) {
+        return responseService.getFailResult(Integer.valueOf(getMessage("accessDenied.code")), getMessage("accessDenied.msg"));
+    }
+
+
 
     // code 정보에 해당하는 메시지를 조회
     private String getMessage(String code) {
