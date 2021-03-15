2142b776a3456b80b6b042144bbbbe0571f0a78c
pebblepark <subling02@gmail.com>
2021-03-16 00:25:48 +0900

    kakao 로그인 페이지 controller

---
+++ src/main/java/com/example/restapi/controller/common/SocialController.java
@@ -0,0 +1,43 @@
+package com.example.restapi.controller.common;
+
+import com.example.restapi.service.user.KakaoService;
+import com.google.gson.Gson;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/social/login")
+public class SocialController {
+
+    private final Environment env;
+
+    @Value("${spring.url.base}")
+    private String baseUrl;
+
+    @Value("${spring.social.kakao.client_id}")
+    private String kakaoClientId;
+
+    @Value("${spring.social.kakao.redirect}")
+    private String kakaoRedirect;
+
+    @GetMapping
+    public ModelAndView socialLogin(ModelAndView mav) {
+        StringBuilder loginUrl = new StringBuilder()
+                .append(env.getProperty("spring.social.kakao.url.login"))
+                .append("?client_id=").append(kakaoClientId)
+                .append("&response_type=code")
+                .append("&redirect_uri=").append(baseUrl).append(kakaoRedirect);
+
+        mav.addObject("loginUrl", loginUrl);
+        mav.setViewName("social/login");
+        return mav;
+    }
+}
